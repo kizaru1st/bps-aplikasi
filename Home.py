4f57ab0fb4ee9f7e7d413f5a8e89dd3b417df95e
Iqbal <89783237+IqbalManazilYuni@users.noreply.github.com>
2023-01-24 09:12:40 +0700

    tanpa watermark

--- Home.py
+++ Home.py
@@ -12,4 +12,11 @@ st.set_page_config(page_title='BPS Provinsi Sumatera Barat',
                    layout='wide')
 
 st.title("title lom ada")
-
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+                """
+st.markdown(hide_st_style, unsafe_allow_html=True)
--- pages/Neraca.py
+++ pages/Neraca.py
@@ -5,6 +5,15 @@ import subprocess
 import sys
 from streamlit_option_menu import option_menu
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+                """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 excel_file = 'UHH.xlsx'
 sheet_name = 'DATA'
 
